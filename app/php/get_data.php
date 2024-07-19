178c1c8702ed0086752388db168f19038543f556
StaceyMarkiv <stasy-9876@mail.ru>
2024-07-19 22:19:42 +0300

    added highlighting users with default car info

--- app/php/get_data.php
+++ app/php/get_data.php
@@ -13,16 +13,22 @@ $result = pg_query($db, "SELECT users.id_user,
                                 education.grade,
                                 
                                 string_agg(cities.city_name, ', ' ORDER BY cities.city_name) AS city,
-                                CASE WHEN users.has_car = true THEN 'blue'
+                                CASE
+                                    WHEN users.has_car = true THEN 'blue'
                                     ELSE NULL
-                                END AS row_color
+                                END AS has_car_color,
+                                CASE
+                                    WHEN cars.car_brand = 'new_car' OR cars.color = 'new_color' THEN 'red'
+                                    ELSE NULL
+                                END AS default_car_color
                         FROM $schema.users
                         JOIN $schema.education ON users.id_grade = education.id_grade
                         LEFT JOIN $schema.user_cities ON users.id_user = user_cities.id_user
                         LEFT JOIN $schema.cities ON cities.id_city = user_cities.id_city
-                        GROUP BY users.id_user, education.id_grade
+                        LEFT JOIN $schema.cars ON users.id_user = cars.id_user
+                        GROUP BY users.id_user, education.id_grade, cars.car_brand, cars.color
                         ORDER BY users.last_name
-                    ") or die('Data load failed:' . pg_last_error());
+                        ") or die('Data load failed:' . pg_last_error());
 
 $rows = [];
 while ($row = pg_fetch_assoc($result)) {
@@ -34,7 +40,8 @@ while ($row = pg_fetch_assoc($result)) {
         'id_grade' => $row['id_grade'],
         'grade' => $row['grade'],
         'city' => $row['city'],
-        'row_color' => $row['row_color'],
+        'has_car_color' => $row['has_car_color'],
+        'default_car_color' => $row['default_car_color'],
     );
 }
 
