dd46d6beb7c757f09e341165f547fbc73d1608e7
StaceyMarkiv <stasy-9876@mail.ru>
2024-08-13 21:39:17 +0300

    amended update_logins.php

--- app/php/update_logins.php
+++ app/php/update_logins.php
@@ -9,7 +9,12 @@ $res = pg_query($db, $db_query1) or die('Data load failed:' . pg_last_error() . 
 $max_id = pg_fetch_all_columns($res, 0)[0];
 
 //получаем параметры из запроса
+$action = $_POST['action'];
 $id = isset($_POST['id']) ? $_POST['id'] : null;
+
+$field_name = isset($_POST['fieldName']) ? $_POST['fieldName'] : null;
+$field_value = isset($_POST['fieldValue']) ? $_POST['fieldValue'] : null;
+
 $login = isset($_POST['login']) ? $_POST['login'] : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
 $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : null;
@@ -23,42 +28,35 @@ if ($id_role === null) {
     $id_role = isset($_POST['role']) ? $_POST['role'] : null;
     $id_role = preg_match('~[0-9]+~', $id_role) ? (int) $id_role : null;        //определяем, что в поле role пришло число
 }
-$action = $_POST['action'];
 
 if ($action == 'add') {
-    $first_name = ($first_name !== '') ? "'" . $first_name . "'": 'NULL';
-    $last_name = ($last_name !== '') ? "'" . $last_name . "'": 'NULL';
+    $first_name = ($first_name !== '') ? $first_name : 'NULL';
+    $last_name = ($last_name !== '') ? $last_name : 'NULL';
 
-    $db_query2 = "INSERT INTO $schema.logins (id, login, password, first_name, last_name, id_role)
-                VALUES ($max_id, '$login', '$password', $first_name, $last_name, $id_role)";
+    $add_query = "INSERT INTO $schema.logins (id, login, password, first_name, last_name, id_role)
+                VALUES ($max_id, $1, $2, $3, $4, $5)";
+
+    //подготовка запроса
+    $result = pg_prepare($db, "add_query", $add_query);
+    //запуск запроса на выполнение
+    $result = pg_execute($db, "add_query", array($login, $password, $first_name, $last_name, $id_role)) or die('Data load failed:' . pg_last_error() . 'sql = ' . $add_query);
 } else if ($action == 'update') {
-    if ($login) {
-        $db_query2 = "UPDATE $schema.logins
-                    SET login='$login'
-                    WHERE id=$id;";
-    } else if ($password) {
-        // $password = password_hash($password, PASSWORD_DEFAULT);
-        $db_query2 = "UPDATE $schema.logins
-                    SET password='$password'
-                    WHERE id=$id;";
-    } else if ($first_name) {
-        $db_query2 = "UPDATE $schema.logins
-                    SET first_name='$first_name'
-                    WHERE id=$id;";
-    } else if ($last_name) {
-        $db_query2 = "UPDATE $schema.logins
-                    SET last_name='$last_name'
-                    WHERE id=$id;";
-    } else if ($id_role) {
-        $db_query2 = "UPDATE $schema.logins
-                    SET id_role=$id_role
-                    WHERE id=$id;";
-    }
-} else if ($action == 'delete') {
-    $db_query2 = "DELETE FROM $schema.logins
-                WHERE id=$id;";
-}
+    $update_query = "UPDATE $schema.logins
+                    SET $field_name = $2
+                    WHERE id=$1;";
 
-pg_query($db, $db_query2) or die('Data load failed:' . pg_last_error() . 'sql = ' . $db_query2);
+    //подготовка запроса
+    $result = pg_prepare($db, "update_query", $update_query);
+    //запуск запроса на выполнение
+    $result = pg_execute($db, "update_query", array($id, $field_value)) or die('Data load failed:' . pg_last_error() . 'sql = ' . $update_query);
+} else if ($action == 'delete') {
+    $delete_query = "DELETE FROM $schema.logins
+                WHERE id=$1;";
+
+    //подготовка запроса
+    $result = pg_prepare($db, "delete_query", $delete_query);
+    //запуск запроса на выполнение
+    $result = pg_execute($db, "delete_query", array($id)) or die('Data load failed:' . pg_last_error() . 'sql = ' . $delete_query);
+}
 
 echo "{'success': true}";
